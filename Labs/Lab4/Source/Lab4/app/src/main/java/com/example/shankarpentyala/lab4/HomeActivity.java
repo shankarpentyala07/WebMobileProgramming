d2da660631e6332b37150c13bcf280706cde2e84
Shankar-Pentyala <shankar.pentyala@ibm.com>
2018-12-09 18:49:22 -0600

    Lab4

---
+++ Labs/Lab4/Source/Lab4/app/src/main/java/com/example/shankarpentyala/lab4/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.example.shankarpentyala.lab4;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassResult;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+
+
+    public void logout(View view) {
+        Intent redirect = new Intent(HomeActivity.this,LoginActivity.class);
+        startActivity(redirect);
+    }
+
+    public void maps(View view) {
+        Intent redirect = new Intent(HomeActivity.this,MapsActivity.class);
+        startActivity(redirect);
+    }
+
+    public void camera(View view) {
+        Intent redirect = new Intent(HomeActivity.this,CameraActivity.class);
+        startActivity(redirect);
+    }
+}
